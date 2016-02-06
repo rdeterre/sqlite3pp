67b4d4b9945717611268dc2237069a26d2e36cd7
TyRoXx <tyroxxxx@gmail.com>
2016-02-06 19:17:25 +0100

    copy DLLs to bin on Windows

--- conanfile.py
+++ conanfile.py
@@ -12,3 +12,7 @@ class SiliciumConan(ConanFile):
 
     def package(self):
         self.copy(pattern="*.hpp", dst="include/sqlite3pp", src="sqlite3pp", keep_path=True)
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
