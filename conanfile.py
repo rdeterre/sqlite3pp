ee8e1d16bdfa912782b1c1d189b063c115321554
TyRoXx <tyroxxxx@gmail.com>
2016-03-16 23:29:48 +0100

    bump version to 0.9

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class SiliciumConan(ConanFile):
     name = "sqlite3pp"
-    version = "0.7"
+    version = "0.9"
     generators = "cmake"
     requires = "silicium/0.9@TyRoXx/master", "sqlite3/3.10.2@TyRoXx/stable"
     url="http://github.com/tyroxx/sqlite3pp"
