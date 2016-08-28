439ec61fe0d8e7a2ff60c4043e6dc052b2689231
Romain Deterre <romain.deterre@gmail.com>
2016-08-28 11:15:51 -0400

    Update to version 1.0.5

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,25 @@
 from conans import ConanFile
 import os
 
-class SiliciumConan(ConanFile):
+class SQLite3ppConan(ConanFile):
     name = "sqlite3pp"
-    version = "0.9"
+    version = "1.0.5"
     generators = "cmake"
-    requires = "silicium/0.9@TyRoXx/master", "sqlite3/3.10.2@TyRoXx/stable"
-    url="http://github.com/tyroxx/sqlite3pp"
+    requires = "sqlite3/3.14.1@rdeterre/stable"
+    url="http://github.com/rdeterre/sqlite3pp"
     license="MIT"
-    exports="sqlite3pp/*"
+    exports="*"
+
+    def source(self):
+        self.run("git clone --branch {} https://github.com/iwongu/sqlite3pp" \
+                 .format(self.version))
 
     def package(self):
-        self.copy(pattern="*.hpp", dst="include/sqlite3pp", src="sqlite3pp", keep_path=True)
+        self.copy(pattern="*", dst="include/sqlite3pp", src="sqlite3pp/headeronly_src", keep_path=True)
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
+
+    def package_info(self):
+        self.cpp_info.includedirs = ['include']
